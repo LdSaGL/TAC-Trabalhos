6561d9bca32e04d4190a81569fd19b5572644f4b
LdSaGL <lucas.sa.licio@gmail.com>
2025-04-27 17:51:27 -0300

    Orientacoes Parte 1

---
+++ T1/S-AES.py
@@ -0,0 +1,13 @@
+"""
+Implementar o algoritmo AES simplificado em Python.
+
+Blocos de 16 bits
+2 rodadas
+
+Op. Básicas:
+AddRoundKey
+SubNibbles (S-Box fixa)
+ShiftRows
+MixColumns (GF(2^4))
+KeyExpansion
+"""
